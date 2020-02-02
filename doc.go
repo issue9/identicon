803432880bcb34f34b1076c272b49d05a4cb335b
caixw <xnotepad@gmail.com>
2020-02-02 21:13:23 +0800

    docs: 调整文件头

--- doc.go
+++ doc.go
@@ -1,6 +1,4 @@
-// Copyright 2015 by caixw, All rights reserved.
-// Use of this source code is governed by a MIT
-// license that can be found in the LICENSE file.
+// SPDX-License-Identifier: MIT
 
 // Package identicon 一个基于 hash 值生成随机图像的包
 //
